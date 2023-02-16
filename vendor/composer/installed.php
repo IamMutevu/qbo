4620db41d0a05a867100aaea458b3d361895358f
iammutevu <ianmutevu96@gmail.com>
2023-02-16 15:38:26 +0300

    Add Quickbooks SDK

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'quickbooks/v3-php-sdk' => array(
+            'pretty_version' => 'v6.1.1',
+            'version' => '6.1.1.0',
+            'reference' => '20ce804a7e643d91b0a8f115e969bf6a0713f64e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../quickbooks/v3-php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
